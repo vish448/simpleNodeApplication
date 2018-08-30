589a29c11b64a604dcf7eda83c8d06a70fac56c1
vishang soni <vishangsoni@gmail.com>
2018-08-30 16:34:58 -0400

    Updated message in App.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000
 const server = http.createServer((req,res)=>{
 	res.statusCode = 200;
 	res.setHeader('Content-Type', 'text/html');
-	res.end('<h1>Hi There, How are you!</h1>');
+	res.end('<h1>Hello World, How are you!</h1>');
 });
 server.listen(port,() => {
 	console.log('Server running at port', +port);
